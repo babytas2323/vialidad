2f6ec00c44b16bb4ed561dfa63b2cbaddd56b90b
pineda <probitsolution@hotmail.com>
2025-10-04 15:53:56 -0600

    Actualizo mapa protector con cambios recientes

--- src/MapaProtector.jsx
+++ src/MapaProtector.jsx
@@ -66,7 +66,7 @@ function MapaProtector() {
       {posicion ? (
         <MapContainer
           center={posicion}
-          zoom={14}
+          zoom={19}
           scrollWheelZoom={true}
           style={{ height: '100%', width: '100%', zIndex: 1 }}
           whenCreated={(mapInstance) => { mapaRef.current = mapInstance; }}
