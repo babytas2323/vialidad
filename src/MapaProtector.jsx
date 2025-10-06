e147b920cb01f517d27decce56f78498ba67b1a3
pineda <probitsolution@hotmail.com>
2025-10-05 18:05:12 -0600

    Actualizo mapa protector con cambios recientes

--- src/MapaProtector.jsx
+++ src/MapaProtector.jsx
@@ -67,17 +67,13 @@ function MapaProtector() {
         <>
           <MapContainer
             center={posicion}
-            zoom={14}
+            zoom={18}
             scrollWheelZoom={true}
             style={{ height: '100%', width: '100%', zIndex: 1 }}
             whenCreated={(mapInstance) => { mapaRef.current = mapInstance; }}
           >
-            <TileLayer attribution="&copy; Google Maps" url="https://mt1.google.com/vt/lyrs=s,r&x={x}&y={y}&z={z}" subdomains={['mt0', 'mt1', 'mt2', 'mt3']}  maxZoom={21} />
-            {/* <TileLayer
-              attribution='&copy; OpenStreetMap'
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                maxZoom={22}
-            />*/}
+         {/*<TileLayer attribution="&copy; Google Maps" url="https://mt1.google.com/vt/lyrs=s,r&x={x}&y={y}&z={z}" subdomains={['mt0', 'mt1', 'mt2', 'mt3']}  maxZoom={21} />*/}
+         <TileLayer attribution='&copy; OpenStreetMap' url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" maxZoom={22} />
 
             {geojsonCalles && <CallesVialidad datos={geojsonCalles} />}
 
